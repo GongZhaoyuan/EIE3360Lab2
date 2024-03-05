c45ca9bef331d724c907008bccffe3e3d06113a1
GongZhaoyuan <140373573+GongZhaoyuan@users.noreply.github.com>
2024-03-05 12:00:23 +0800

    web

--- Assets/Scripts/Player/PlayerInventory.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-	
-public class PlayerInventory : MonoBehaviour
-{
-    public bool hasKey;         // Whether or not the player has the key.
-}
